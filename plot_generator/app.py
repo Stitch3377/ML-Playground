4dd4abed70509e65536bdcc6c36ef7efa083a06e
Ivanka SooHoo <ivankavsh@gmail.com>
2025-08-27 20:08:10 -0400

    plot generator app

---
+++ plot_generator/app.py
@@ -0,0 +1,44 @@
+"""A simple Streamlit app to generate plot ideas using a fine-tuned GPT2 language model."""
+
+import streamlit as st
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+MODEL_NAME = "distilgpt2"
+LATEST_CHECKPOINT = "./output/model/checkpoint-1750"
+
+@st.cache_resource
+class AppModel:
+    """A simple class to load the model and generate text."""
+    def __init__(self):
+        self.tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+        self.tokenizer.pad_token = self.tokenizer.eos_token
+
+        self.model = AutoModelForCausalLM.from_pretrained(LATEST_CHECKPOINT)
+
+    def generate(self, user_prompt: str):
+        """Generate a book description based on the user prompt."""
+        inputs = self.tokenizer(user_prompt, return_tensors="pt").to("cpu")
+
+        outputs = self.model.generate(
+            inputs.input_ids,
+            max_new_tokens=100,
+            do_sample=True,
+            top_k=50,
+            top_p=0.95
+        )
+
+        output_string = self.tokenizer.batch_decode(outputs)
+
+        return output_string
+
+
+model = AppModel()
+
+prompt = st.text_area("Enter the beginning of plot...")
+clicked = st.button("✨ Generate!")
+
+if clicked:
+    generated_email = model.generate(prompt)[0]
+
+    chat_message = st.chat_message("assistant")
+    chat_message.markdown(generated_email)
